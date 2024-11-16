feb57376bf106cc62c417670df04b9549f2b1ce2
ratiahhh <raatihhh23@gmail.com>
2024-11-16 17:22:15 +0700

    Create cisco_config.sh

---
+++ cisco_config.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Variabel Konfigurasi
+SWITCH_IP="192.168.31.35"       # IP Cisco Switch (diubah sesuai permintaan)
+USER_SWITCH="root"             # Username SSH untuk Cisco Switch
+PASSWORD_SWITCH="root"         # Password untuk Cisco Switch
+VLAN_ID=10
+VLAN_NAME="VLAN10"
+INTERFACE="e0/1"               # Port yang digunakan di Cisco Switch
+
+# Warna untuk tampilan
+RED='\033[31m'
+GREEN='\033[32m'
+CYAN='\033[36m'
+RESET='\033[0m'
+
+# Fungsi untuk menampilkan pesan sukses atau gagal
+print_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "${GREEN}✔ Konfigurasi Cisco Switch berhasil!${RESET}"
+  else
+    echo -e "${RED}✘ Gagal mengonfigurasi Cisco Switch!${RESET}"
+    exit 1
+  fi
+}
+
+echo -e "${CYAN}Memulai konfigurasi Cisco Switch...${RESET}"
+
+# Login ke Cisco Switch dan lakukan konfigurasi VLAN
+echo -e "${CYAN}Membuat VLAN $VLAN_ID ($VLAN_NAME) di Cisco Switch...${RESET}"
+sshpass -p "$PASSWORD_SWITCH" ssh -o StrictHostKeyChecking=no $USER_SWITCH@$SWITCH_IP <<EOF
+enable
+configure terminal
+vlan $VLAN_ID
+name $VLAN_NAME
+exit
+interface $INTERFACE
+switchport mode access
+switchport access vlan $VLAN_ID
+exit
+end
+write memory
+EOF
+
+# Cek status konfigurasi
+print_status
