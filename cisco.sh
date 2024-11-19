6bdf3327c222bcb0d8198559bd123853d1da0b02
ratiahhh <raatihhh23@gmail.com>
2024-11-19 14:03:10 +0700

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+# Variabel untuk konfigurasi Cisco Switch
+SWITCH_IP="192.168.31.2"       # Ganti dengan IP Switch Cisco
+USER_SWITCH="admin"            # Ganti dengan username switch
+PASSWORD_SWITCH="password"     # Ganti dengan password switch
+VLAN_ID=10                     # ID VLAN yang akan dibuat
+VLAN_NAME="VLAN10"             # Nama VLAN
+INTERFACE="e1"                 # Interface yang akan dikonfigurasi
+
+# Kirim perintah ke Cisco Switch menggunakan sshpass dan SSH
+sshpass -p "$PASSWORD_SWITCH" ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no $USER_SWITCH@$SWITCH_IP << EOF
+enable
+configure terminal
+vlan $VLAN_ID
+name $VLAN_NAME
+exit
+interface $INTERFACE
+switchport mode access
+switchport access vlan $VLAN_ID
+exit
+end
+write memory
+EOF
+
+# Cek apakah konfigurasi berhasil
+if [ $? -eq 0 ]; then
+  echo "✔ Cisco Switch berhasil dikonfigurasi pada interface $INTERFACE."
+else
+  echo "✘ Gagal mengonfigurasi Cisco Switch. Periksa koneksi SSH atau konfigurasi VLAN."
+  exit 1
+fi
