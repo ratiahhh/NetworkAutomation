3a45e6bd08f5b815f44cb9b775c101da62699a40
ratiahhh <raatihhh23@gmail.com>
2024-11-25 19:31:10 +0700

    Update mikrotik.sh

--- mikrotik.sh
+++ mikrotik.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-# Variabel untuk konfigurasi MikroTik
-MIKROTIK_IP="192.168.157.135"       # Ganti dengan IP MikroTik
-USER_MIKROTIK="admin"            # Ganti dengan username MikroTik
-PASSWORD_MIKROTIK="123"     # Ganti dengan password MikroTik
-VLAN_ID=10                       # ID VLAN
-VLAN_NAME="VLAN10"               # Nama VLAN
-INTERFACE="eth1"                 # Interface yang akan dikonfigurasi
+# Instal sshpass jika belum tersedia
+echo "Memastikan sshpass terpasang..."
+apt update && apt install -y sshpass
 
-# Kirim perintah ke MikroTik menggunakan sshpass dan SSH
-sshpass -p "" ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no $USER_MIKROTIK@$MIKROTIK_IP << EOF
+# Variabel konfigurasi
+MIKROTIK_IP="192.168.31.254"   # IP untuk MikroTik
+USERNAME="admin"              # Username MikroTik
+PASSWORD="password"           # Password MikroTik
+
+# Skrip konfigurasi MikroTik
+echo "Mengirim konfigurasi ke MikroTik..."
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USERNAME@$MIKROTIK_IP << EOF
 /interface vlan
-add name=$VLAN_NAME vlan-id=$VLAN_ID interface=$INTERFACE
+add interface=ether2 name=vlan10 vlan-id=10
+
 /ip address
-add address=192.168.31.254/24 interface=$VLAN_NAME
+add address=192.168.31.254/24 interface=vlan10
+
+/ip dhcp-client
+add interface=ether1 disabled=no
+
+/ip route
+add dst-address=0.0.0.0/0 gateway=192.168.31.1
+
+/system reboot
 EOF
 
-# Cek apakah konfigurasi berhasil
-if [ $? -eq 0 ]; then
-  echo "✔ MikroTik berhasil dikonfigurasi pada interface $INTERFACE."
-else
-  echo "✘ Gagal mengonfigurasi MikroTik. Periksa koneksi SSH atau konfigurasi VLAN."
-  exit 1
-fi
+echo "Konfigurasi MikroTik selesai! MikroTik akan reboot untuk menerapkan konfigurasi."
