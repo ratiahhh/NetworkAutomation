9f1727fc7e9811e30717ce4b6b6dcceb9425605a
ratiahhh <raatihhh23@gmail.com>
2024-11-26 09:23:13 +0700

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
-# IP PNET dan VLAN konfigurasi
-PNET_IP="192.168.157.129"
-PNET_MASK="255.255.255.0"
-VLAN_ID=10
-VLAN_IP="192.168.31.2"
-VLAN_MASK="255.255.255.0"
+# Script Konfigurasi Cisco Switch dengan Netmiko
+echo "Memulai Otomasi Konfigurasi Cisco Switch"
 
-echo "Mengonfigurasi Cisco Switch..."
+cat <<EOF > cisco_config.py
+from netmiko import ConnectHandler
 
-cat <<EOT > cisco_config.txt
-enable
-configure terminal
-!
-! Konfigurasi IP PNET pada VLAN 1 (Default VLAN)
-interface vlan 1
-ip address $PNET_IP $PNET_MASK
-no shutdown
-exit
-!
-! Konfigurasi VLAN 10
-vlan $VLAN_ID
-name VLAN_10
-exit
-!
-! Tetapkan port ke VLAN 10
-interface e0/1
-switchport mode access
-switchport access vlan $VLAN_ID
-no shutdown
-exit
-!
-! IP Address untuk VLAN 10
-interface vlan $VLAN_ID
-ip address $VLAN_IP $VLAN_MASK
-no shutdown
-exit
-!
-end
-write memory
-exit
-EOT
+# Konfigurasi perangkat Cisco
+cisco_device = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.157.129',  # IP PNET Cisco
+    'username': 'admin',
+    'password': 'admin',
+    'secret': 'admin',
+}
 
-echo "Konfigurasi Cisco selesai. Periksa koneksi PNET dan VLAN."
+# Koneksi ke perangkat Cisco
+net_connect = ConnectHandler(**cisco_device)
+net_connect.enable()
+
+# Konfigurasi Switch
+commands = [
+    'vlan 10',
+    'name VLAN_10',
+    'interface vlan 10',
+    'ip address 192.168.31.2 255.255.255.0',
+    'no shutdown',
+    'interface gigabitEthernet 0/1',
+    'switchport mode access',
+    'switchport access vlan 10',
+    'exit'
+]
+output = net_connect.send_config_set(commands)
+print(output)
+
+# Simpan konfigurasi
+net_connect.save_config()
+
+# Tutup koneksi
+net_connect.disconnect()
+EOF
+
+echo "Konfigurasi Cisco selesai."
