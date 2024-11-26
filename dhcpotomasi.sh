ca9a996298c19ee245c7c2e178848777bdc4fb40
ratiahhh <raatihhh23@gmail.com>
2024-11-26 08:12:57 +0700

    Update dhcpotomasi.sh

--- dhcpotomasi.sh
+++ dhcpotomasi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Otomasi Dimulai
-echo "Memulai Otomasi Ubuntu DHCP Server dan SSH"
+echo "Memulai Otomasi Ubuntu DHCP Server, VLAN, dan IP PNET"
 
 # Repo Kartolo
 cat <<EOF | sudo tee /etc/apt/sources.list
@@ -24,6 +24,8 @@ network:
       dhcp4: yes
     eth1:
       dhcp4: no
+      addresses:
+        - 192.168.157.128/24 # Tambahkan IP PNET ke eth1
   vlans:
     eth1.10:
       id: 10
@@ -31,12 +33,17 @@ network:
       addresses:
         - 192.168.31.1/24
 EOT
+
+# Terapkan konfigurasi Netplan
+echo "Menerapkan konfigurasi jaringan..."
 netplan apply
 
 # Instalasi ISC DHCP Server
+echo "Menginstal ISC DHCP Server..."
 sudo apt install isc-dhcp-server -y
 
 # Konfigurasi DHCP Server
+echo "Mengonfigurasi DHCP Server untuk VLAN..."
 cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf
 subnet 192.168.31.0 netmask 255.255.255.0 {
   range 192.168.31.2 192.168.31.254;
@@ -54,24 +61,29 @@ echo 'INTERFACESv4="eth1.10"' | sudo tee /etc/default/isc-dhcp-server
 sudo systemctl restart isc-dhcp-server
 
 # Aktifkan IP Forwarding
+echo "Mengaktifkan IP forwarding..."
 sudo sed -i '/^#net.ipv4.ip_forward=1/s/^#//' /etc/sysctl.conf
 sudo sysctl -p
 
 # NAT Masquerading
+echo "Menambahkan NAT Masquerading..."
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo apt install iptables-persistent -y
 
 # Instal SSH Server
+echo "Menginstal SSH Server..."
 sudo apt install openssh-server -y
 sudo systemctl enable ssh
 sudo systemctl restart ssh
 
 # Buka Port SSH di Firewall
+echo "Mengizinkan koneksi SSH melalui firewall..."
 sudo ufw allow ssh
 sudo ufw enable
 
 # Instalasi Tambahan untuk Netmiko
+echo "Menginstal Netmiko untuk pengaturan otomatisasi..."
 sudo apt install python3 python3-pip -y
 pip3 install netmiko
 
-echo "Konfigurasi Ubuntu selesai. DHCP Server, SSH, dan NAT siap digunakan."
+echo "Konfigurasi Ubuntu selesai. IP PNET, VLAN, DHCP Server, dan SSH siap digunakan."
