e02bfb855a5407b8a7459ee1ee79a558735e4994
ratiahhh <raatihhh23@gmail.com>
2025-05-23 22:04:35 +0700

    Update dns_server.sh

--- dns_server.sh
+++ dns_server.sh
@@ -1,25 +1,65 @@
-# Menginstall dan Mengkonfigurasi DNS-SERVER
+#!/bin/bash
 
-echo "[+] Install dan konfigurasi DNS"
-apt install bind9 -y
+echo "===================================="
+echo "===     MENGINSTAL BIND9         ==="
+echo "===================================="
+apt update && apt install -y bind9 dnsutils
 
-cat >> /etc/bind/named.conf.local << EOF
+echo "===================================="
+echo "===  MENYIAPKAN DIREKTORI ZONA  ==="
+echo "===================================="
+mkdir -p /etc/bind/zones
+
+echo "=============================================="
+echo "=== KONFIGURASI ZONA DI NAMED.CONF.LOCAL   ==="
+echo "=============================================="
+echo "Konfigurasi berhasil..."
+cat <<EOF > /etc/bind/named.conf.local
+zone "itclub.com" {
+    type master;
+    file "/etc/bind/db.itclub.com";
+};
+
 zone "networking.net" {
     type master;
     file "/etc/bind/db.networking.net";
 };
 
-zone "itclub.com" {
+zone "10.10.10.in-addr.arpa" {
     type master;
-    file "/etc/bind/db.itclub.com";
+    file "/etc/bind/db.192";
 };
 EOF
 
-cp /etc/bind/db.local /etc/bind/db.networking.net
-cp /etc/bind/db.local /etc/bind/db.itclub.com
+echo "=============================================="
+echo "=== MEMBUAT FILE ZONA UNTUK ITCLUB.COM...  ==="
+echo "=============================================="
+cat <<EOF > /etc/bind/db.itclub.com
+\$TTL    604800
+@       IN      SOA     itclub.com. root.itclub.com. (
+                              2         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         602800 )       ; Negative Cache TTL
+;
+        IN      NS      itclub.com.
+        IN      A       10.10.10.1
+www     IN      A       10.10.10.1
+EOF
 
-sed -i 's/localhost/networking.net./' /etc/bind/db.networking.net
-sed -i 's/127.0.0.1/10.10.1.1/' /etc/bind/db.networking.net
-
-sed -i 's/localhost/itclub.com./' /etc/bind/db.itclub.com
-sed -i 's/127.0.0.1/10.10.1.1/' /etc/bind/db.itclub.com 
+echo "====================================================="
+echo "=== MEMBUAT FILE ZONA UNTUK NETWORKING.NET...    ==="
+echo "====================================================="
+cat <<EOF > /etc/bind/db.networking.net
+\$TTL    604800
+@       IN      SOA     networking.net. root.networking.net. (
+                              2         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         602800 )       ; Negative Cache TTL
+;
+        IN      NS      networking.net.
+        IN      A       10.10.10.1
+EOF
